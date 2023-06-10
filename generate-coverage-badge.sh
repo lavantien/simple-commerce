222637aecc2de092821f503ba92b9e4d8209f1ea
lavantien <lavantien96@gmail.com>
2023-06-10 20:26:51 +0700

    better coverage calculation

--- generate-coverage-badge.sh
+++ generate-coverage-badge.sh
@@ -2,9 +2,15 @@
 COVERAGE=`go tool cover -func=coverage.out | grep total: | grep -Eo '[0-9]+\.[0-9]+'`
 echo $COVERAGE
 COLOR=orange
-if (( $(echo "$COVERAGE <= 50" | bc -l) )) ; then
+if (( $(echo "$COVERAGE < 50" | bc -l) )) ; then
     COLOR=red
+    elif (( $(echo "$COVERAGE > 60" | bc -l) )); then
+    COLOR=yellow
+    elif (( $(echo "$COVERAGE > 70" | bc -l) )); then
+    COLOR=yellowgreen
     elif (( $(echo "$COVERAGE > 80" | bc -l) )); then
     COLOR=green
+    elif (( $(echo "$COVERAGE == 100" | bc -l) )); then
+    COLOR='brightgreen'
 fi
 curl "https://img.shields.io/badge/coverage-$COVERAGE%25-$COLOR" > coverage.svg
